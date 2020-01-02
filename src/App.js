93f7e2c81411de3ab348bd7b1502d643bb83fe0a
puneethdc99 <58505120+puneethdc99@users.noreply.github.com>
2020-01-02 22:47:34 +0530

    dropdown
    dropdown

---
+++ src/App.js
@@ -0,0 +1,46 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { Dropdown, DropdownButton } from "react-bootstrap";
+import "./App.css";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { isLoaded: false, eData: [] };
+  }
+
+  getAlltheIndex = () => {
+    fetch("http://34.89.89.247:9002/_aliases?pretty=true")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          eData: Object.keys(json)
+        });
+        console.log(this.state.eData);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 onClick={this.getAlltheIndex}>Click Me</h1>
+          <select>
+            {this.state.eData.map(data => (
+              <option value="data.value">{data.toString()}</option>
+            ))}
+          </select>
+          <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
+            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
+            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          </DropdownButton>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
