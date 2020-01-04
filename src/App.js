23da3623ff4d2bf8c911e3f99ad5d1737a9a89b4
puneethdc99 <58505120+puneethdc99@users.noreply.github.com>
2020-01-04 23:40:04 +0530

    get all msgs
    get all msgs

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
-import { Dropdown, DropdownButton } from "react-bootstrap";
+import { Dropdown, DropdownButton, ListGroup } from "react-bootstrap";
 import "./App.css";
 
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { isLoaded: false, eData: [] };
+    this.state = { isLoaded: false, eData: [], sData: [] };
   }
 
   getAlltheIndex = () => {
@@ -21,12 +21,25 @@ class App extends Component {
       });
   };
 
+  getAlltheData = () => {
+    fetch("http://34.89.89.247:9002/edge-logs/_search")
+      .then(res => res.json())
+      .then(json => {
+        this.setState({
+          isLoaded: true,
+          sData: json.hits.hits
+        });
+        console.log(this.state.sData);
+      });
+  };
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 onClick={this.getAlltheIndex}>Click Me</h1>
+          <h1 onClick={this.getAlltheData}>Click Me 2</h1>
           <select>
             {this.state.eData.map(data => (
               <option value="data.value">{data.toString()}</option>
@@ -37,6 +50,13 @@ class App extends Component {
               <Dropdown.Item>{data.toString()}</Dropdown.Item>
             ))}
           </DropdownButton>
+          <ListGroup as="ul">
+            {this.state.sData.map(data => (
+              <ListGroup.Item as="li" active>
+                {data._source.MESSAGE}
+              </ListGroup.Item>
+            ))}
+          </ListGroup>
         </header>
       </div>
     );
